2138ce242b326ee5323acdc14b6c8add54f07f6f
Felipe Hubner <feliperodrigueshubner@hotmail.com>
2024-07-25 21:56:08 -0300

    fix file length

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 header('Content-Type: application/json');
 
-// CONTROLE DE TAMANHO DA REQUISIÇÃO
-if ($_SERVER['CONTENT_LENGTH'] > $postMaxSize) { // 10 MB em bytes
-    http_response_code(400);
-    die(json_encode(['error' => 'Dados ultrapassam o limite de 10MB']));
-}
 
 require_once __DIR__ . '/env.php';
 require_once __DIR__ . '/router.php';
+
+// CONTROLE DE TAMANHO DA REQUISIÇÃO
+if ($_SERVER['CONTENT_LENGTH'] > $postMaxSize) { // 10 MB em bytes
+    http_response_code(400);
+    die(json_encode(['error' => 'Dados ultrapassam o limite de 10MB'.$_SERVER['CONTENT_LENGTH']]));
+}
 
 define('UPLOAD_DIR', __DIR__ . '/uploads/');
 
